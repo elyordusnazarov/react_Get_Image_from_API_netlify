85673445fcf962f087c9f9901afcbc7dc0990afd
elyordusnazarov <elyor.dusnazarov@gmail.com>
2023-05-20 19:01:23 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+import Header from "./layout/Header"
+import Meme from "./components/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
